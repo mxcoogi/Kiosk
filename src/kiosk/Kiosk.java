948a6d10475ed0c393fa50332cd7626f6d3f6d22
mxcoogi <mxcoogi@gmail.com>
2025-03-07 16:14:49 +0900

    feat : Add Interactable interface

--- src/kiosk/Kiosk.java
+++ src/kiosk/Kiosk.java
@@ -1,4 +1,18 @@
 package kiosk;
 
-public class Kiosk {
+public class Kiosk implements Interactable{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void input() {
+
+    }
+
+    @Override
+    public void output() {
+
+    }
 }
